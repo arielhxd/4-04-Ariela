72211f1e361958f2cb3182fdfd1327f5a069fe54
arelhxd <arielalintang259@gmail.com>
2022-01-24 15:31:43 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Ariela | Home</title>
+</head>
+<body>
+    <h1> Halaman About </h1>
+    <h3> Ariela Lintang </h3>
+    <p> 3103120031@student.smktelkom-pwt.sch.id </p>
+    <img src="images/ariela.jpeg" alt="ariela" width="2000x">
+
+</body>
+</html>
